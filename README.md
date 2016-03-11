dc61716b7643422b7f978503c36e0102a7a5ac02
worldsky007 <616502576@qq.com>
2016-03-11 13:41:42 +0800

    firstry

---
+++ README.md
@@ -0,0 +1,10 @@
+利用Flask框架快速搭建博客
+==============
+
+本项目是利用Flask搭建博客。
+
+1.ssh-config.sh为搭建环境的脚本。可在Linux环境下使用
+
+2.Flask-micblog为搭建环境所使用文件
+
+3.micblog为搭建的博客文件
---
+++ ssh_config.sh
@@ -0,0 +1,6 @@
+sudo apt-get install tree
+sudo apt-get update
+# ??pip
+sudo apt-get install python-pip
+# ??pip??flask-micblog/requirement.txtrequirement.txt???
+sudo pip install -i http://mirrors.aliyuncs.com/pypi/simple -r flask-micblog/requirement.txt
